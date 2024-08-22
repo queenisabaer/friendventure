dee06a6ee34c11f8b846c87f75d99a1978dcc44a
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-22 10:32:31 +0000

    feat: add routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.css'
 import NavBar from './components/NavBar'
-
+import Container from 'react-bootstrap/Container'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   
@@ -8,6 +9,13 @@ function App() {
     <>
       <div className={styles.App}>
         <NavBar />
+        <Container className={styles.Main}>
+          <Routes>
+            <Route exact path='/' element= {<h1>Home Page</h1>} />
+            <Route exact path='/signup' element={<h1>Sign Up</h1> }/>
+            <Route path="*" element={<p>Sorry, Page not found</p>} />
+          </Routes>
+        </Container>
       </div>
     </>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,51 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
-import logo from "../assets/FriendVenture_logo.png"
-import styles from "../styles/NavBar.module.css"
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../assets/FriendVenture_logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand><img className={styles.Logo} src={logo} alt="friendventure logo" height="60px"/></Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img
+              className={styles.Logo}
+              src={logo}
+              alt="friendventure logo"
+              height="60px"
+            />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="ml-auto text-center">
-            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-user-pen"></i>Sign Up</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Log in</Nav.Link>
+            <Nav.Link
+              end
+              as={NavLink}
+              className={({ isActive }) => isActive ? "active" : ""}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              className={({ isActive }) => isActive ? "active" : ""}
+              to="/signup"
+            >
+              <i className="fa-solid fa-user-pen"></i>Sign Up
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              className={({ isActive }) => isActive ? "active" : ""}
+              to="/login"
+            >
+              <i className="fa-solid fa-right-to-bracket"></i>Log in
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>    
+    </Navbar>
   );
 };
 
