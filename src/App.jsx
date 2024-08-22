3ad20403eeb67a12b5140c084fb096ae2f4ada0d
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-22 08:02:29 +0000

    style: add styles for index.html, and navbar with module.css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import styles from './App.module.css'
 import NavBar from './components/NavBar'
 
 
@@ -6,7 +6,7 @@ function App() {
   
   return (
     <>
-      <div className='App'>
+      <div className={styles.App}>
         <NavBar />
       </div>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,22 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
 import logo from "../assets/FriendVenture_logo.png"
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand><img src={logo} alt="logo" height="60px"/></Navbar.Brand>
+        <Navbar.Brand><img className={styles.Logo} src={logo} alt="friendventure logo" height="60px"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Sign Up</Nav.Link>
-            <Nav.Link href="#link">Log in</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="ml-auto text-center">
+            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+            <Nav.Link><i className="fa-solid fa-user-pen"></i>Sign Up</Nav.Link>
+            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Log in</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+    </Navbar>    
   );
 };
 
