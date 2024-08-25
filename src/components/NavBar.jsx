bea5994bf013d1b786ae2fa4d643528dfdd773ba
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-25 09:16:08 +0000

    feat: add icons to NavBar for logged in users, add logout functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,74 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import logo from "../assets/FriendVenture_logo.png";
 import styles from "../styles/NavBar.module.css";
-import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { NavLink, Link } from "react-router-dom";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const setCurrentUser = useSetCurrentUser()
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("/dj-rest-auth/logout/");
+      setCurrentUser(null);
+      showMessage("success", "Successfully signed out!");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loggedInIcons = (
+    <>
+      <NavDropdown
+        title={
+          <span id={styles.dropdownMenu}>
+            <i className="fa-solid fa-dharmachakra"></i>
+            FriendVentures
+          </span>
+        }
+      >
+        <NavDropdown.Item
+          as={Link}
+          className={({ isActive }) => (isActive ? `${styles.NewActive}` : "")}
+          to="/add"
+        >
+          <i className="fa-solid fa-calendar-plus"></i>Add
+        </NavDropdown.Item>
+        <NavDropdown.Item
+          as={Link}
+          className={({ isActive }) => (isActive ? `${styles.NewActive}` : "")}
+          to="/bookmarks"
+        >
+          <i className="fa-solid fa-heart"></i>Bookmarks
+        </NavDropdown.Item>
+        <NavDropdown.Item
+          as={Link}
+          className={({ isActive }) => (isActive ? `${styles.NewActive}` : "")}
+          to="/upcoming"
+        >
+          <i className="fa-solid fa-calendar-check"></i>Upcoming
+        </NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link
+        as={NavLink}
+        className={({ isActive }) => (isActive ? `${styles.NewActive}` : "")}
+        to="/profile"
+      >
+        <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={30} />
+      </Nav.Link>
+      <Nav.Link
+        as={NavLink}
+        className={({ isActive }) => (isActive ? `${styles.NewActive}` : "")}
+        to="/"
+        onClick={handleSignOut}
+      >
+        <i className="fa-solid fa-right-from-bracket"></i>Log Out
+      </Nav.Link>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <Nav.Link
@@ -45,7 +107,9 @@ const NavBar = () => {
             <Nav.Link
               end
               as={NavLink}
-              className={({ isActive }) => (isActive ? `${styles.NewActive}` : "")}
+              className={({ isActive }) =>
+                isActive ? `${styles.NewActive}` : ""
+              }
               to="/"
             >
               <i className="fas fa-home"></i>Home
